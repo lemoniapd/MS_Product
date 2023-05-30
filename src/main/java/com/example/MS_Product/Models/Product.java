fb676b54ba7e9c54f688556d9b1abcac64c82a13
lisapel <lisa.peldan@gmail.com>
2023-05-30 15:45:43 +0200

    Changes by lemoniapd

--- src/main/java/com/example/MS_Product/Models/Product.java
+++ src/main/java/com/example/MS_Product/Models/Product.java
@@ -1,5 +1,7 @@
 package com.example.MS_Product.Models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +16,10 @@ public class Product {
     @Id
     @GeneratedValue
     protected Long id;
+    @Min(1)
     protected double price;
+
+    @Size(min=2,max=50)
     protected String name;
 
     public Product(double price, String name) {

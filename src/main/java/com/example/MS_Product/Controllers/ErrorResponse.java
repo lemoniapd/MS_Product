ba922b525b9e835508fcd6e27fc693907b04f7f9
lisapel <lisa.peldan@gmail.com>
2023-05-30 15:44:32 +0200

    Added error handling

--- src/main/java/com/example/MS_Product/Controllers/ErrorResponse.java
+++ src/main/java/com/example/MS_Product/Controllers/ErrorResponse.java
@@ -1,16 +1,10 @@
 package com.example.MS_Product.Controllers;
-
 import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-// Example of a custom error response
+import lombok.NoArgsConstructor;// Example of a custom error response
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
